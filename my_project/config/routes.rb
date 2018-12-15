e2baa6c4c183cf99d9d20a36b2c066bb57b399e9
Chris Walquist <chris@walquist.net>
2018-12-15 16:28:59 -0600

    After running this (and perhaps a b rake db:migrate): $ b bin/rake alchemy_devise:install:migrations $ b bin/rake alchemy:install

--- my_project/config/routes.rb
+++ my_project/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Alchemy::Engine => '/alchemy'
+
   get '/my_page' => 'my_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
